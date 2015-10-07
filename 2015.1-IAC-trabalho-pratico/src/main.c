61c59da0b990f3a314263ecbbf12aa642c0b551b
João Correia <lucaskm98@gmail.com>
2015-10-07 18:23:48 -0300

    Estrutura inicial do código

--- 2015.1-IAC-trabalho-pratico/src/main.c
+++ 2015.1-IAC-trabalho-pratico/src/main.c
@@ -4,30 +4,28 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-int main (int argc, char *argv[], char *envp[]) {
+int main (int argc, char *argv[]) {
 
-int pid ; /* identificador de processo */
-
-pid = fork () ; /* replicação do processo */
-
-if ( pid < 0 ) { /* se fork não funcionou */
-	perror ("Erro: ") ;
-	exit (-1) ; /* encerra o processo com código de erro -1 */ 
-}
-else if( pid > 0 ) /* se sou o processo pai*/ 
-{
-	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-	//TODO após 10 segundos de execução, mate o proceso filho
-}
-else /* senão, sou o processo filho*/ 
-{
-	//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória:
-
-}
-perror ("Erro: ") ; /* execve não funcionou */
-
-printf ("Tchau !\n") ;
-exit(0) ; /* encerra o processo com sucesso (código 0) */ 
-
+	int pid ; /* identificador de processo */
+	
+	pid = fork () ; /* replicação do processo */
+	
+	if ( pid < 0 ) { /* se fork não funcionou */
+		perror ("Erro: ") ;
+		exit (-1) ; /* encerra o processo com código de erro -1 */ 
+	}else if( pid > 0 ){	
+		//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
+		//TODO após 10 segundos de execução, mate o proceso filho
+	}else{
+		
+		if((strcmp(argv[1],"cpu") == 0)){
+			printf("CPU\n");
+			//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
+		}else if((strcmp(argv[1],"cpu-mem") == 0)){
+			printf("CPU-MEM\n");
+			//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória:	
+		}
+		
+	}
+	return 0;
 }
