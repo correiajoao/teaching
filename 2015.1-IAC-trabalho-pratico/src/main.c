baa09bac9f7d4993703876619f21efd49779768e
João Correia <lucaskm98@gmail.com>
2015-10-18 14:15:20 -0300

    Comentários em inglês adicionados

--- 2015.1-IAC-trabalho-pratico/src/main.c
+++ 2015.1-IAC-trabalho-pratico/src/main.c
@@ -5,134 +5,149 @@
 #include <sys/wait.h> 
 
 
-/* Descrição: Essa função executa um teste de uso intenso de cpu e memória
-   Paramentros: 
-   Retorno: void
-*/
-void cpumemorytest(){
-	for(;;){          
-		malloc(sizeof(10000));
-	}
-}
-/* Descrição: Essa função executa um teste de uso intenso de cpu
-   Paramentros:
-   Retorno: void
+/* Description: This function executes an intense cpu use
+   Parameters:
+   Return: void
 */
 void cputest(){
 	for(;;){
 	}
 } 
 
+/* Description: This function executes an intense use of cpu and memory 
+   Parameters:
+   Return: void
+*/
+void cpumemorytest(){
+	for(;;){          
+		malloc(sizeof(10000));
+	}
+}
+
+/* Description: This is the main function 
+   Parameters: int argc, char *argv[]
+   Return: int
+*/
 int main (int argc, char *argv[]) {	
 	
-	int i,pid,core;
-	float memory, cpu;
-	char dataignored[50],commandline[50];
+	int i;	//Controller
+	int pid;	//Process identification
+	int core;	//Number of cpu's cores
+	float cpu;	//Cpu usage
+	float memory;	//Memory usage
+	char dataignored[50];	//Aux to reading data
+	char commandline[50];	//Monitoring commands 	
 	
+	//fork(2) - Linux man page - http://linux.die.net/man/2/fork
 	pid = fork(); 
-	//Se o fork não funcionou esse trecho de código é executado
+	
+	//If an error occurred in the fork
 	if ( pid < 0 ) { 
 		perror ("Erro: ") ;
 			exit (-1) ;  
 	
-	//Se o processo for o pai esse trecho de código é executado
+	//If is the father process
 	}else if( pid > 0 ){  
 		
-		printf("Monitoramento iniciado.\n");
+		printf("Starting monitoring\n");
 		core = get_nprocs_conf();
-		printf("Núcleos identificados: %d\n", core);
+		printf("Identified cores: %d\n", core);
 
-		FILE *scanTerminal; // Ponteiro para a leitura do retorno dos comandos passados ao terminal
-		FILE *out; 			// Ponteiro para o arquivo de saida dos dados gerados
-				
-		if((strcmp(argv[1],"cpu") == 0)){ //Código de monitoramento do uso de CPU	
+		FILE *scanTerminal; 
+		FILE *output; 		
+		
+		//Code for monitoring cpu
+		if((strcmp(argv[1],"cpu") == 0)){  	
 			
-			out = fopen("out.txt", "a");
-			fprintf(out, "%s","\n");
+			output = fopen("output.txt", "a");
+			fprintf(output, "%s","\n");
 			
 			for(i=0;i<10;i++){
 				printf("Time: %ds    ", i);
-				fprintf(out, "%s%d    ","Time: ",i);
+				fprintf(output, "%s%d    ","Time: ",i);
 		
-				//Gerando linha de comando para monitoramento de 	: ps -p [pid] -o %cpu
+				//Generating command line for monitoring cpu: ps -p [pid] -o %cpu
 				sprintf(commandline, "%s%d%s", "ps -p ", pid," -o %cpu"); 	
 				
-				//Abrindo o processo terminal com um pipe
+				//Running the command and capturing the output
 				//popen(3) - Linux man page - http://linux.die.net/man/3/popen
 				scanTerminal = popen(commandline,"r");
 				
-				//Lendo dados do retorno
+				//Reading data
 				fscanf(scanTerminal,"%s %f", dataignored, &cpu);
 				
-				//Imprime os dados no terminal e no arquivo de saida
+				//Printing data in the terminal and output file
 				printf("CPU: %.2f%%\n", cpu/core);
-				fprintf(out, "CPU: %.2f%%\n", cpu/core);
+				fprintf(output, "CPU: %.2f%%\n", cpu/core);
 				fclose(scanTerminal);
 				
-				//Espera 1s para o controle dos 10s
+				//Waits one second
 				sleep(1);
 			}
 			
-			//Após 10s, morte do filho
+			//After 10 seconds the child is dead
 			//KILL(1) - Linux man page - http://linux.die.net/man/1/kill
 			kill(pid, SIGKILL);
-			//O arquivo de gravação é fechado
-			fclose(out);
+			//Close de outpu file
+			fclose(output);
 
 		}else if ((strcmp(argv[1],"cpu-mem") == 0)){
 			
-			out = fopen("out.txt", "a");
-			fprintf(out, "%s","\n");
+			output = fopen("output.txt", "a");
+			fprintf(output, "%s","\n");
 			
-			//Se o argv[1] for igual a 'cpu-mem', o pai irá monitorar o consumo de CPU e memória					
+			//Code for monitoring cpu and memory					
 			for(i=0;i<10;i++){	
 				printf("Time: %ds    ", i);
-				fprintf(out, "%s%d    ","Time: ",i);
+				fprintf(output, "%s%d    ","Time: ",i);
 				
-				//Gerando linha de comando para monitoramento de CPU: ps -p [pid] -o %cpu
+				//Generating command line for monitoring cpu: ps -p [pid] -o %cpu
 				sprintf(commandline, "%s%d%s", "ps -p ", pid," -o %cpu"); 	
 				
-				//Abrindo o processo terminal com um pipe
+				//Running the command and capturing the output
 				//popen(3) - Linux man page - http://linux.die.net/man/3/popen
 				scanTerminal = popen(commandline,"r");
 				
-				//Lendo dados do retorno
+				//Reading data  
 				fscanf(scanTerminal,"%s %f", dataignored, &cpu);
 				fclose(scanTerminal);
-				printf("CPU: %.2f%%    ", cpu/core);
-				fprintf(out,"CPU: %.2f%%    ", cpu/core);
 				
-				//Gerando linha de comando para o monitoramento de memória: cat /proc/[pid]/status | grep 'VmSize'
+				//Printing data in the terminal and output file
+				printf("CPU: %.2f%%    ", cpu/core);
+				fprintf(output,"CPU: %.2f%%    ", cpu/core);
+				
+				//Generating command line for monitoring memory: cat /proc/[pid]/status | grep 'VmSize'
 				sprintf(commandline,"%s%d%s","cat /proc/",pid,"/status | grep 'VmSize'");
 				
-				//Abrindo o processo terminal com um pipe
+				//Running the command and capturing the output
 				//popen(3) - Linux man page - http://linux.die.net/man/3/popen
 				scanTerminal = popen(commandline, "r");
 				
-				//Lendo dados de retorno
+				//Reading data 
 				fscanf(scanTerminal,"%s %f %s", dataignored, &memory, dataignored);
-				
+			
+				//Printing data in the terminal and output file
 				printf("Memory: %.2f KB\n", memory);
-				fprintf(out,"Memory: %.2f KB\n", memory);
+				fprintf(output,"Memory: %.2f KB\n", memory);
 				fclose(scanTerminal);
 				
-			    //Espera 1s para o controle dos 10s
+			    //Waits one second
 				sleep(1);
 			}
 			
-			//Após 10s, morte do filho
+			//After ten seconds the child is dead
 			//kill(1) - Linux man page - http://linux.die.net/man/1/kill
 			kill(pid, SIGKILL);
-			fclose(out);
+			fclose(output);
 		
 		}	
 		
-	//Se o processo for o filho esse trecho de código é executado	
+	//If is the child process	
 	}else if(pid == 0){
 
-		if((strcmp(argv[1],"cpu") == 0)){ //Se o parâmetro de entrada for 'cpu' inicia-se o teste de cpu 	
+		if((strcmp(argv[1],"cpu") == 0)){
 			cputest();
-		}else if((strcmp(argv[1],"cpu-mem") == 0)){ //Se o parâmetro de entrada for 'cpu-mem' inicia-se o teste de cpu e memória
+		}else if((strcmp(argv[1],"cpu-mem") == 0)){
 			cpumemorytest();
 		}		
 	
